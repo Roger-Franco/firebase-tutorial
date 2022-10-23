26077fd045e10466247bb97f7cabd7902930aab2
Roger-Franco <rogerfrancogalt@gmail.com>
2022-10-23 12:10:01 -0400

    CRUD started. getUsers

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Getting Started with Create React App
+## PedroTech
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { collection, getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { db } from './firebase-config'
 
 function App() {
+  const [users, setUsers] = useState([])
+  const usersCollectionRef = collection(db, "users")
+
+  const createUser = async () => {
+
+  }
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef)
+      // data.forEach((doc) => {
+      //   return console.log(doc.data(), doc.id);
+      // });
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    }
+
+    getUsers()
+
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="Name..." />
+      <input type="number" placeholder="Age..." />
+      <button onClick={createUser} >Create User </button>
+      {users.map((user) => {
+        return (
+          <div>
+            <h1>Name: {user.name || user.Name}</h1>
+            <h1>Age: {user.age || user.Age}</h1>
+          </div>
+        )
+      })}
     </div>
   );
 }
